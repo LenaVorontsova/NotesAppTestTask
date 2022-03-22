5bdcb309a617be66a53a638c058ef7c7d8d59d66
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-03-22 18:54:05 +0400

    add: add save note and show it in the table

--- NotesAppTestTask/NotesAppTestTask/NoteDetailVC.swift
+++ NotesAppTestTask/NotesAppTestTask/NoteDetailVC.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreData
 
 class NoteDetailVC: UIViewController {
 
@@ -10,6 +11,22 @@ class NoteDetailVC: UIViewController {
     }
 
     @IBAction func saveNote(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Note", in: context)
+        let newNote = Note(entity: entity!, insertInto: context)
+        
+        newNote.id = noteList.count as NSNumber
+        newNote.desc = descriptionTV.text
+        newNote.title = titleTF.text
+        do {
+            try context.save()
+            noteList.append(newNote)
+            navigationController?.popViewController(animated: true)
+        }
+        catch {
+            print("Context save error")
+        }
     }
     
 }
--- NotesAppTestTask/NotesAppTestTask/NoteTableView.swift
+++ NotesAppTestTask/NotesAppTestTask/NoteTableView.swift
@@ -1,5 +1,25 @@
 import UIKit
 
+var noteList = [Note]()
 class NoteTableView: UITableViewController {
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let noteCell = tableView.dequeueReusableCell(withIdentifier: "noteCellID", for: indexPath) as! NoteCell
+        
+        let thisNote: Note!
+        thisNote = noteList[indexPath.row]
+        
+        noteCell.descriptionLabel.text = thisNote.desc
+        noteCell.titleLabel.text = thisNote.title
+        
+        return noteCell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noteList.count
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
 }
