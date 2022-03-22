302ae6ae29104d23d31a8f98b79c712edfe0e931
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-03-22 18:37:57 +0400

    add: add NoteCell, NoteTableView and ViewController for details page, create note entity

---
+++ NotesAppTestTask/NotesAppTestTask/NoteDetailVC.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class NoteDetailVC: UIViewController {
+
+    @IBOutlet weak var descriptionTV: UITextView!
+    @IBOutlet weak var titleTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func saveNote(_ sender: Any) {
+    }
+    
+}
+
